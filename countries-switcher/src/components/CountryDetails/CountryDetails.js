46a57526d37fa625045bec056e41fca6211641f8
jpromanonet <programming@jpromano.net>
2021-11-25 23:16:44 -0300

    CountryDetails.js

--- countries-switcher/src/components/CountryDetails/CountryDetails.js
+++ countries-switcher/src/components/CountryDetails/CountryDetails.js
@@ -42,5 +42,61 @@ class CountryDetails extends Component {
             .catch(error => console.log(error));
     };
 
+    componentDidMount() {
+        document.title = this.props.match.params.countryName;
+        this.fetchCountryDetails();
+    }
+    componentDidUpdate(prevProps) {
+        document.title = this.props.match.params.countryName;
+        if(prevProps.location.key !== this.props.location.key){
+            this.fetchCountryDetails();
+        }
+    }
+
+    render() {
+        const { darkMode, appModeChanger, totalCountries, homePage } = this.props;
+        const { countryDetails, notFoundStatus } = this.state;
+        return (
+            <React.Fragment>
+                <header
+                    className={`${styles.countryDetails} ${darkMode ? `dark` : `light`}`}    
+            >
+                <NavBar
+                    darkMode={darMode}
+                    homePage={homePage}
+                    appModeChanger={appModeChanger}
+                />
+                <Link
+                    to={homePage}
+                    className={`${styles.backButton} ${
+                        darkMode ? "dark darkElements" : "Light lightElements"
+                    }`}
+                >
+                    <i className="fas fa-arrow-left"></i>
+                </Link>
+            </header>
+            <main
+                className={`${styles.countryDetails} ${styles.main} ${
+                    darkMode ? `dark` : `light`
+                }`}
+            >
+                {}
+                { CountryDetails ? (
+                    notFoundStatus ? (
+                        <p className={styles.error}>
+                            Sorry, we don't have any idea about the thing you're looking for...
+                        </p>
+                    ) : (
+                        <React.Fragment>
+                            <div className={styles.error}>
+                                
+                            </div>
+                        </React.Fragment>
+                    )
+                )}
+            </main>
+            </React.Fragment>
+        )
+    }
     
 }
