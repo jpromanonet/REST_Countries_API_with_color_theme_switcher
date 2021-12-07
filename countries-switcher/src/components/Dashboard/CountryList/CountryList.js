378c87f9942105b55c6484253dfb7a75823fcdf5
jpromanonet <programming@jpromano.net>
2021-12-06 23:34:48 -0300

    Country list

--- countries-switcher/src/components/Dashboard/CountryList/CountryList.js
+++ countries-switcher/src/components/Dashboard/CountryList/CountryList.js
@@ -10,8 +10,25 @@ const countryItemCreator = (filteredCountries, currentPage, darkMode, homePage) 
     return filteredCountries
         .slice(currentPage * 8, currentPage * 8 + 8)
         .map(country => (
-            <CountryItem Key={country.name} {...country} darkMode={darkMode} homePage={homePage} />
+            <CountryItem key={country.name} {...country} darkMode={darkMode} homePage={homePage} />
         ));
 };
 
 // Component declaration
+const CountryList = ({ filteredCountries, darkMode, totalCountries, homePage, scrollTo}) => {
+    // Declate the state properties using react hooks
+    const [currentPage, setCurrentPage] = useState(0);
+    const [totalPages, setTotalPages] = useState(1);
+
+    // Update totalpages whenever the filterd countries' list changes
+    useEffect(() => {
+        // Calculate the number of totalpages if there are more than 8 countries available
+        // otherwise set it to 1 
+        const tempTotalPages =
+            filteredCountries.length > 0
+                ? MediaDeviceInfo.ceil(filteredCountries.length / 8)
+                : 1;
+        setTotalPages(tempTotalPages);      
+    }, [filteredCountries]);
+    // Update the currentPage whenever the filtered countries' list changes
+}
